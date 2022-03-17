e75069582fe3ad7a85eadae238feb66787583637
celinux <c3linux@gmail.com>
2022-03-17 17:51:13 +0400

    added publish action

--- backend/search/admin.py
+++ backend/search/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Card
-class CardAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_on', 'updated_on')
 
+@admin.action(description='Publish selected commands')
+def make_published(modeladmin, request, queryset):
+    queryset.update(status='1')
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on', 'updated_on', 'status')
+    actions = [make_published]
 
 
 # Register your models here.
 admin.site.register(Card, CardAdmin)
+
+
