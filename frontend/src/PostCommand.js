5a41af35895cf25802ffffaafc52c8c940e1503e
Rəşad Rzayev <reshadrzayev00@gmail.com>
2022-03-17 16:25:04 +0400

    Updated

--- frontend/src/PostCommand.js
+++ frontend/src/PostCommand.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PostCommand = () => {
+
+    const [title, setTitle] = useState()
+    const [content, setContent] = useState()
+    
+    const postCommands = async (e) => {
+        e.preventDefault();
+        try {
+            axios({
+                method: 'POST',
+                url: 'http://1d63-185-96-126-12.ngrok.io/api/cards/',
+                headers: { 'content-type': 'application/json' },
+                credentials: 'include',
+                data: {
+                    title,
+                    content
+                }
+            })
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div className='post-command'>
-            <form method="post">
+            <form onSubmit={postCommands} method="post">
                 <div className="input">
-                    <input type="text" placeholder='Command Title'  />
+                    <input type="text" placeholder='Command Title' onChange={(e) => setTitle(e.target.value)} />
                     <span className="top spaninp"></span>
                     <span className="bottom spaninp"></span>
                 </div>
                 <br /> <br />
                 <div className="input">
-                    <input type="text" placeholder='Command Content' />
+                    <input type="text" className='content-input' placeholder='Command Content' onChange={(e) => setContent(e.target.value)} />
                     <span className="top spaninp"></span>
                     <span className="bottom spaninp"></span>
                 </div>
-                
-
-
-                <button>Post</button>
+                <button type='submit'>Post</button>
             </form>
         </div>
     )
