968cd2b11fe0b770160c7a088f89af3715af43ea
Rəşad Rzayev <reshadrzayev00@gmail.com>
2022-03-19 03:10:39 +0400

    post-command updated

--- frontend/src/PostCommand.js
+++ frontend/src/PostCommand.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const PostCommand = () => {
 
-    const [title, setTitle] = useState()
-    const [content, setContent] = useState()
-    
+    const [title, setTitle] = useState("")
+    const [content, setContent] = useState("")
+    const [status, setStatus] = useState()
+    const [ready, setReady] = useState(false)
+    const [statusContent, setStatusContent] = useState(false)
+
     const postCommands = async (e) => {
         e.preventDefault();
         try {
-            axios({
+            const response = await axios({
                 method: 'POST',
-                url: 'http://1d63-185-96-126-12.ngrok.io/api/cards/',
+                url: 'http://164.92.177.109:8000/api/cards/',
                 headers: { 'content-type': 'application/json' },
                 credentials: 'include',
                 data: {
@@ -19,27 +22,45 @@ const PostCommand = () => {
                     content
                 }
             })
+            if (response) {
+                setStatus("Command added!")
+                setStatusContent(true)
+            }
+            setTimeout(() => {
+                setReady(false)
+            }, 1500, setReady(true))
+
         }
         catch (error) {
-            console.log(error.message);
+            setStatus("Error occured!")
+            setStatusContent(false)
+            setTimeout(() => {
+                setReady(false)
+            }, 1500, setReady(true), setStatus("Error occured!"))
         }
+        setTitle('')
+        setContent('')
     }
 
     return (
         <div className='post-command'>
+            <div className={`status ${ready === true ? "status-active" : "status"}`}>
+                <p className={`status-content ${statusContent === true ? "status-ok" : "status-error"}`}>{status}</p>
+                <button className='cancel' onClick={() => { setReady(false) }}>x</button>
+            </div>
             <form onSubmit={postCommands} method="post">
                 <div className="input">
-                    <input type="text" placeholder='Command Title' onChange={(e) => setTitle(e.target.value)} />
+                    <input type="text" placeholder='Command Title' onChange={(e) => setTitle(e.target.value)} required value={title} />
                     <span className="top spaninp"></span>
                     <span className="bottom spaninp"></span>
                 </div>
                 <br /> <br />
                 <div className="input">
-                    <input type="text" className='content-input' placeholder='Command Content' onChange={(e) => setContent(e.target.value)} />
+                    <input type="text" className='content-input' placeholder='Command Content' onChange={(e) => setContent(e.target.value)} required value={content} />
                     <span className="top spaninp"></span>
                     <span className="bottom spaninp"></span>
                 </div>
-                <button type='submit'>Post</button>
+                <button className='submit' type='submit'>Post</button>
             </form>
         </div>
     )
